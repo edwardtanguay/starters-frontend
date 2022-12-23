c9d5b6b0c78b77c9a2b87d8dcc1e3ee0c7a99b3e
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-24 00:57:36 +0100

    about site

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,7 +1,23 @@
 export const PageAbout = () => {
 	return (
 		<div className="page pageAbout">
-			<p>This is the About page.</p>
+			<img src="images/edward_tanguay.jpg" />
+			<div className="info">
+				<p>
+					Hello, my name is Edward Tanguay, I make websites that help
+					web developers be more effective.
+				</p>
+				<p>You might also like these:</p>
+				<div className="item">
+					<a href="https://jobs.tanguay.eu">jobs.tanguay.eu</a> -
+					developer jobs in Germany with links to learn required
+					skills
+				</div>
+				<div className="item">
+					<a href="https://edwardtanguay.vercel.app/howtos">howtos</a> -
+				over 600 howtos with step-by-step instructions, mostly MERN stack	
+				</div>
+			</div>
 		</div>
 	);
 };
