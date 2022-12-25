551ddf03f78e99affe1aedb85cb7dbb7fb733387
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-25 23:06:16 +0100

    period

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 	return (
 		<div className="App">
 			<h1>Starters, Templates and Frameworks</h1>
-			<h2>Base projects to get you up-and-coding fast!</h2>
+			<h2>Base projects to get you up-and-coding fast.</h2>
 			<nav>
 				<NavLink to="/list">Show All</NavLink>
 				<NavLink to="/about">About</NavLink>
