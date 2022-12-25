e9be85b047f4ee69b0dbf5cce10d3b49b52be37c
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-25 22:45:55 +0100

    with helmet

--- src/components/SingleStarter.tsx
+++ src/components/SingleStarter.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { IStarter } from '../interfaces';
 import { convertMarkdownToHtml } from '../tools';
 import { RepositoryButtons } from './RepositoryButtons';
+import { Helmet } from 'react-helmet';
 
 interface IProps {
 	singleStarter: IStarter;
@@ -12,6 +13,24 @@ export const SingleStarter = ({ singleStarter }: IProps) => {
 
 	return (
 		<>
+			<Helmet>
+				<title>{singleStarter.title}</title>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<meta property="og:title" content="Developer Jobs in Germany" />
+				<meta property="og:type" content="site" />
+				<meta
+					property="og:description"
+					content="Daily developer Jobs in Germany listed with information on skills needed and links to learn these skills"
+				/>
+				<meta
+					property="og:image"
+					content="https://jobs.tanguay.eu/images/screenshot-jobsite.png"
+				/>
+				<meta property="og:url" content="https://jobs.tanguay.eu" />
+			</Helmet>
 			<div className="singleStarter">
 				<img
 					src={singleStarter.imageUrl}
